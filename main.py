0d5286f265938d31f60732b50e192ada063f1097
HariharanWTS <hariharanwisetechsource@gmail.com>
2024-08-01 15:31:05 +0530

    email_sent_update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 import pandas as pd
-from redmail import gmail, EmailSender
+from redmail import gmail
 import customtkinter
 import re
 import webview
@@ -16,7 +16,7 @@ from CTkMessagebox import CTkMessagebox
 import openpyxl
 
 
-#
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -28,9 +28,12 @@ class App(customtkinter.CTk):
         self.excel_file_to_mail_header_list = []
         self.scrollable_frame_switches = []
         self.title("EmailZapprz")
-        self.static_attachment_file_path_list = []
+        self.attachment_file_path_list = []
+        self.static_attachment_file_count = 0
         self.html_state = ["Dynamic","Static"]
         self.current_html_state = None
+        self.individual_attachments_header = []
+        self.email_subject = ""
         # self.iconbitmap("WTS.ico")
         self.resizable(False, False)  
         # Disable window resizing
@@ -130,8 +133,6 @@ class App(customtkinter.CTk):
         self.entry_dynamic.grid(row=0, column=0, columnspan=2, padx=(40, 1), pady=(10, 10), sticky="ew")
 
 
-
-
         self.static_upload_button=CTkButton(self.static_frame,text="Upload",font=CTkFont(family="times",size=20,weight="bold"),hover_color='#808080',hover=True,fg_color='#3b8ed0',height=10,border_color="dark",text_color="#1c1c1c",corner_radius=10, command=self.upload_static_html_file)
         self.static_upload_button.grid(row=0, column=2, columnspan=3, padx=(10, 40), pady=(10, 10), sticky="ew")
         self.static_sub_button=CTkButton(self.static_frame,text="Submit",font=CTkFont(family="times",size=20,weight="bold"),hover_color='#808080',hover=True,fg_color='#3b8ed0',height=10,border_color="dark",text_color="#1c1c1c",corner_radius=10, command=self.upload_static_custom_file)
@@ -190,6 +191,7 @@ class App(customtkinter.CTk):
             self.static_preview_frame_function()
 
     def attachment_sub_function(self):
+        self.email_subject = self.subject_entry.get()
         if len(self.subject_entry.get()) >= 1 and self.subject_entry.get().strip() != "":                
             self.list_frame_show_call()
         else:
@@ -232,6 +234,13 @@ class App(customtkinter.CTk):
         
         return data_dict
     
+    def get_checkbox_values(self):
+        data_dict = []
+        for checkbox in self.dynamic_scrollable_frame_checkbox:
+            if checkbox.get() == 1:
+                data_dict.append(checkbox.cget("text"))
+        self.individual_attachments_header = data_dict 
+    
     def sub_attach_function(self):
         print("Attach entry")
         self.static_frame.grid_forget()
@@ -247,7 +256,7 @@ class App(customtkinter.CTk):
             checkbox = customtkinter.CTkCheckBox(master=self.dynamic_scroll_checkbox_frame,text=key)
             checkbox.grid(row=index, column=0, pady=20, padx=20, sticky="w")
             self.dynamic_scrollable_frame_checkbox.append(checkbox)
-        self.dynamic_scroll_checkbox_submit = customtkinter.CTkButton(self.subject_attach_frame, text="Submit",width=10)
+        self.dynamic_scroll_checkbox_submit = customtkinter.CTkButton(self.subject_attach_frame, text="Submit",width=10, command=self.get_checkbox_values)
         self.dynamic_scroll_checkbox_submit.grid(row=3, column=0, padx=(200, 0), pady=(20, 20), sticky="nsew")
 
         # params_variable= re.findall(r"\{\{(\w+)\}\}",str(self.html_full_content))
@@ -410,9 +419,10 @@ class App(customtkinter.CTk):
             messagebox.showwarning("Error",f"Please select the file size upto 25MB\nYour selected file size:{total_file_size/1048576:.2f}MB")
         else:
             messagebox.showinfo("File Selected", "Successfully uploaded")
-            self.static_attachment_file_path_list = list(file_path)
-                
-            
+            for static_path in file_path:
+                if os.path.exists(static_path):
+                    self.attachment_file_path_list.append(static_path)
+            self.static_attachment_file_count = len(self.attachment_file_path_list)            
 
     def upload_file(self):
         file_path = filedialog.askopenfilename(
@@ -534,16 +544,18 @@ class App(customtkinter.CTk):
                                         continue
                                 try:
                                     print(index, row.iloc[0])
+                                    for path in self.individual_attachments_header:
+                                        if os.path.exists(rf"{row[path]}"):
+                                            self.attachment_file_path_list.append(rf"{row[path]}")
                                     recipient_email = row.iloc[0] # Assuming your Excel file has a column named 'Email'
                                     gmail.username =   email_data.iloc[0]# Notification mail sent to registered mail of customer
                                     gmail.password = email_data.iloc[1]
-                                    gmail.send(subject = "Checking subject4",
+                                    gmail.send(subject = self.email_subject,
                                                 receivers = [recipient_email],
                                                 html =self.html_full_content,
                                     body_params=self.evaluate_body_params(row),
-
-                                    attachments=self.static_attachment_file_path_list)
-
+                                    attachments=self.attachment_file_path_list)
+                                    self.attachment_file_path_list = self.attachment_file_path_list[:self.static_attachment_file_count]
                                     self.excel_file_df_to_mail['FromMail'] =  email_data.iloc[0] 
                                     self.excel_file_df_to_mail['MailStatus'] =  "Completed"
                                     break  
